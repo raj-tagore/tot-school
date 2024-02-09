cb7163839126358dcb3af5fe3f03e3c2aac87d73
Raj Tagore <rajtagore780@gmail.com>
2024-02-09 23:25:37 +0000

    handled new registration

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,6 +15,15 @@ class DashboardController extends Controller
         $user_id = auth()->id(); // Assuming the user is authenticated
         $totalTally = TotalTally::where('user_id', $user_id)->first();
 
+        if ($totalTally === null) {
+            // Handle the case where no object is found
+            // For example, create a new TotalTally object or set a default value
+            $totalTally = new TotalTally();
+            $totalTally->user_id = $user_id;
+            // Set other properties as required
+            $totalTally->save();
+        }
+
         $topUsersByCalls = TotalTally::orderByDesc('calls')->take(5)->get()->map(function ($user) {
             $user->name = User::find($user->user_id)->name;
             return $user;
